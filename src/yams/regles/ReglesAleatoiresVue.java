2c5872d4fc01edb7a939831082c2fa351567fec7
nicolas <nicolas@nicolas-pc>
2013-06-29 18:30:19 +0200

    ajout de la classe gérant les régles aléatoire et de la page html contenant les règles

---
+++ src/yams/regles/ReglesAleatoiresVue.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package yams.regles;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author nicolas
+ */
+public class ReglesAleatoiresVue extends JFrame {
+
+    public ReglesAleatoiresVue() {
+        throw new UnsupportedOperationException("Not yet supported");
+        //TODO intégrer la page html regles_simples.html dans un JEditorPane
+        //voir plus de détails ici: http://www.developpez.net/forums/d39898/java/interfaces-graphiques-en-java/awtswing/strategieswingafficher-du-html-avec-java/
+    }
+}
