ce3af9b0fb85be02a4f0947a72bc38015c56c04c
nicolas <nicolas@nicolas-pc>
2013-07-02 12:07:42 +0200

    mise en place des règles de façon à ce qu'elles soient affichable par le client jar

--- src/yams/regles/ReglesAleatoiresVue.java
+++ src/yams/regles/ReglesAleatoiresVue.java
@@ -5,15 +5,16 @@
 package yams.regles;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
 import javax.swing.JEditorPane;
 import javax.swing.JFrame;
-import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
+import javax.swing.text.EditorKit;
+import javax.swing.text.html.HTMLEditorKit;
+import javax.swing.text.html.StyleSheet;
 
 /**
  *
@@ -28,15 +29,13 @@ public class ReglesAleatoiresVue extends JFrame {
         pan.setLayout(new BorderLayout());
         
         StringBuffer nom;
-        nom = saisieTexte("src/yams/regles/regles_simples.html");
+        nom = saisieTexte();
         
         JEditorPane jep = new JEditorPane("text/html", new String(nom));
         jep.setEditable(false);
-        JScrollBar jsbv = new JScrollBar(JScrollBar.VERTICAL);
         jep.setPreferredSize(new Dimension(500, 800));
         
         JScrollPane jsp = new JScrollPane(jep);
-        jsp.setVerticalScrollBar(jsbv);
             
         pan.add(jsp, BorderLayout.CENTER);
         
@@ -45,18 +44,95 @@ public class ReglesAleatoiresVue extends JFrame {
         this.setVisible(true);
     }
     
-    private StringBuffer saisieTexte(String nomFichier){
-       File fichier = new File(nomFichier);
+    private StringBuffer saisieTexte(){
 	StringBuffer texte = new StringBuffer();
-	
-	try {
-	Scanner lecteur = new Scanner(fichier);
-	
-	while (lecteur.hasNext()) texte.append(lecteur.nextLine());
-	}
-	catch (FileNotFoundException exc) {
-	System.err.println("Fichier inexistant " + exc);
-	}
+        
+        texte.append("<h1 align='center' color='red'>Règles du Yam's aléatoire</h1>");
+        texte.append("<p>Au Yam’s le nombre de joueur n'est pas fixé, pour des raisons techniques ce logiciel ne gère que 10 joueurs maximum.</p>");
+        texte.append("<p>Le but est, pour chaque joueur, de remplir une grille en totalisant un maximum de points. Pour remplir la grille il faut lancer des dés et réaliser des figures.</p>");
+        texte.append("<p>Chaque joueur joue à tour de rôle. À chaque tour le joueur dispose de trois lancés de cinq dés. À l'issu des deux premiers lancés il peut:</p>");
+        texte.append("<ul>");
+        texte.append("  <li>garder ses 5 dés et remplir une case de sa grille,</li>");
+        texte.append("    <li>écarter une partie des dés et relancer les autre.</li>");
+        texte.append("</ul>");
+        texte.append("<p>Pour garder les 5 dés il suffit de cliquer sur le bouton 'fin du tour'.<br/>Pour écarter un dé, il suffit de cliquer sur la case à cocher se trouvant à sa gauche</p>");
+        texte.append("<p>À l'issu du troisième lancé, ou lorsqu'il choisit de garder ses 5 dés, il doit remplir une case de sa grille. "
+                + "Si le joueur choisit une case alors qu'il n'a pas fait la figure demandé il ne marque pas de point et ne pourra pas retenter la figure "
+                + "plus tard, elle est retirée des figures à obtenir.</p>");
+        texte.append("<p>Il y a 12 figures à réaliser:</p>");
+        texte.append("<table border='1' align='center'>");
+        texte.append("    <thead>");
+        texte.append("        <tr>");
+        texte.append("            <th>Nom</th>");
+        texte.append("            <th>Description</th>");
+        texte.append("            <th>Points</th>");
+        texte.append("        </tr>");
+        texte.append("    </thead>");
+        texte.append("    <tbody>");
+        texte.append("        <tr>");
+        texte.append("            <td>1</td>");
+        texte.append("            <td>Maximum de 1</td>");
+        texte.append("            <td>Somme des 1</td>");
+        texte.append("        </tr>");
+        texte.append("        <tr>");
+        texte.append("            <td>2</td>");
+        texte.append("            <td>Maximum de 2</td>");
+        texte.append("            <td>Somme des 2</td>");
+        texte.append("        </tr>");
+        texte.append("        <tr>");
+        texte.append("            <td>3</td>");
+        texte.append("            <td>Maximum de 3</td>");
+        texte.append("            <td>Somme des 3</td>");
+        texte.append("        </tr>");
+        texte.append("        <tr>");
+        texte.append("            <td>4</td>");
+        texte.append("            <td>Maximum de 4</td>");
+        texte.append("            <td>Somme des 4</td>");
+        texte.append("        </tr>");
+        texte.append("        <tr>");
+        texte.append("            <td>5</td>");
+        texte.append("            <td>Maximum de 5</td>");
+        texte.append("            <td>Somme des 5</td>");
+        texte.append("        </tr>");
+        texte.append("        <tr>");
+        texte.append("            <td>6</td>");
+        texte.append("            <td>Maximum de 6</td>");
+        texte.append("            <td>Somme des 6</td>");
+        texte.append("        </tr>");
+        texte.append("        <tr>");
+        texte.append("            <td>+</td>");
+        texte.append("            <td>Maximum possible</td>");
+        texte.append("            <td>Somme des dés</td>");
+        texte.append("        </tr>");
+        texte.append("        <tr>");
+        texte.append("            <td>-</td>");
+        texte.append("            <td>Minimum possible</td>");
+        texte.append("            <td>Somme des dés</td>");
+        texte.append("        </tr>");
+        texte.append("        <tr>");
+        texte.append("            <td>Suite</td>");
+        texte.append("            <td>5 dés qui se suivent</td>");
+        texte.append("            <td>20</td>");
+        texte.append("        </tr>");
+        texte.append("        <tr>");
+        texte.append("            <td>Full</td>");
+        texte.append("            <td>3+2 dés identiques</td>");
+        texte.append("            <td>30</td>");
+        texte.append("        </tr>");
+        texte.append("        <tr>");
+        texte.append("            <td>Carré</td>");
+        texte.append("            <td>4 dés identiques</td>");
+        texte.append("            <td>40</td>");
+        texte.append("        </tr>");
+        texte.append("        <tr>");
+        texte.append("            <td>Yam's</td>");
+        texte.append("            <td>5 dés identiques</td>");
+        texte.append("            <td>50</td>");
+        texte.append("        </tr>");
+        texte.append("    </tbody>");
+        texte.append("</table>");
+        texte.append("<p>Remarque: Si un joueur totalise 60 points ou plus avec les 6 premières lignes du tableau il gagne un bonus de 30 points.</p>");
+        
 	return texte; 
     }
 }
