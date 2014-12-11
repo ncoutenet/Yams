2653c6e181f7678f341520c60d074f6f31ca0f5c
nicolas <nicolas@nicolas-pc>
2014-12-11 14:28:49 +0100

    mise en place d'une fonction de reset des meilleurs scores

--- src/yams/folder/DataFolder.java
+++ src/yams/folder/DataFolder.java
@@ -6,14 +6,7 @@
 
 package yams.folder;
 
-import java.io.BufferedInputStream;
-import java.io.EOFException;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -285,5 +278,35 @@ public class DataFolder{
             }
         }
         return result;
+    }
+    
+    /*
+     * Permet d'effacer les scores
+     */
+    public void resetScores(int mode){
+        String f;
+        
+        switch(mode){
+            case Yams.MODELIBRE:
+                f = new String(this.LIBRE);
+                break;
+            case Yams.MODEMONTANT:
+                f = new String(this.MONTANT);
+                break;
+            case Yams.MODEDESCENDANT:
+                f = new String(this.DESCENDANT);
+                break;
+            default:
+                System.err.println("Mode inconnu");
+                return; // mode incorrect, annulation de la fonction
+        }
+        
+        try{
+            FileWriter fw = new FileWriter(new File(this._dirName + f));
+            fw.close();
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+        
     }
 }
